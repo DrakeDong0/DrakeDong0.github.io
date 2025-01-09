c17a7ee4585aca46fbdfceef1ce281759359a752
Drake <drakewdong@gmail.com>
2025-01-08 22:14:06 -0500

    update icons

--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -8,10 +8,10 @@ import WorkExperience from '../../Components/WorkSlide/WorkSlide'
 
 import aws from '../../assets/beanstalk.png';
 import silai_logo from '../../assets/work_logos/silai_logo.png';
-import games_instutite_logo from '../../assets/work_logos/games_instutite_logo.png' 
+import games_instutite_logo from '../../assets/work_logos/games_instutite_logo.png'
 
-function WorkContent(){
-    return(
+function WorkContent() {
+    return (
         <div className="work-container">
             <LazyLoad className="section-title fadeBot">Work Experience</LazyLoad>
             <div className="tech-bar">
@@ -21,29 +21,29 @@ function WorkContent(){
                 <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"><img className="tech-img" alt-text="Javascript icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" /></a>
                 <a target="_blank" href="https://flask.palletsprojects.com/en/3.0.x/"><img className="tech-img" alt-text="Flask icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original.svg" /></a>
                 <a target="_blank" href="https://git-scm.com/"><img className="tech-img" alt-text="Git icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" /></a>
-                <a target="_blank" href="https://code.visualstudio.com/"><img className="tech-img" alt-text="VScode icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg" /></a>
-                <a target="_blank" href="https://www.figma.com/"><img className="tech-img" alt-text="Figma icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg"  /></a>
-                <a target="_blank" href="https://www.mysql.com/"><img className="tech-img" alt-text="Mysql icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg"  /></a>
+                <a target="_blank" href="https://www.figma.com/"><img className="tech-img" alt-text="Figma icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" /></a>
+                <a target="_blank" href="https://www.mysql.com/"><img className="tech-img" alt-text="Mysql icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg" /></a>
                 <a target="_blank" href="https://aws.amazon.com/elasticbeanstalk/"><img className="tech-img" alt-text="AWS Beanstalk icon" src={aws} /></a>
                 <a target="_blank" href="https://www.mongodb.com/"><img className="tech-img" alt-text="Mongodb icon" src=" https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg" /></a>
                 <a target="_blank" href="https://go.dev/"><img className="tech-img" alt-text="Golang icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/go/go-original.svg" /></a>
                 <a target="_blank" href="https://www.docker.com/"><img className="tech-img" alt-text="Docker icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg" /></a>
-                <a target="_blank" href="https://www.portainer.io/"><img className="tech-img" alt-text="Portainer icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/portainer/portainer-original.svg"/></a>
+                <a target="_blank" href="https://www.portainer.io/"><img className="tech-img" alt-text="Portainer icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/portainer/portainer-original.svg" /></a>
+                <a target="_blank" href="https://tailwindcss.com/docs/installation"><img className="tech-img" alt-text="Tailwind icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original-wordmark.svg" /></a>
 
-            </div> 
+            </div>
             <div className="work-exp-container">
-            <WorkExperience
+                <WorkExperience
                     logo={games_instutite_logo}
                     name="The Rhetoricon Project"
                     role="Full Stack Developer"
                     date="Sept 2024 - Dec 2024"
                     responsibilities={[
                         "- Led development of GoFigure, responsible for architecture, design, and implementation of new features.",
-                        "- Wrote new API endpoints using Go and developed MongoDB queries.",
+                        "- Wrote new API endpoints using Golang and developed MongoDB queries.",
                         "- Configured, routed, and deployed project site with Docker and Portainer.",
-                        "- Developed a new database backup utility allowing updates without remigrating data.",
-                        "- Helped overhaul frontend website using React.js."
-                      ]}
+                        "- Developed a new database utility allowing backups and updates without remigration.",
+                        "- Helped overhaul frontend website using React.tsx and Tailwind."
+                    ]}
                 />
                 <WorkExperience
                     logo={silai_logo}
@@ -56,15 +56,15 @@ function WorkContent(){
                         "- Designed new REST API endpoints using Flask",
                         "- Deployed applications to AWS Beanstalk and managed databases on AWS RDS.",
                         "- Developed a gyroscope verification tool with TensorFlow to validate camera input data accuracy."
-                      ]}
+                    ]}
                 />
             </div>
         </div>
     );
 }
 
-export default function (){
-    return(
+export default function () {
+    return (
         <>
             <WorkContent></WorkContent>
             <Header></Header>
